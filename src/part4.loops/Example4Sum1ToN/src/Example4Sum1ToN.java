57b9b1bbce31f2da6fedf3d97294b1d93f4424d8
nosatmcc <tacongson2003@gmail.com>
2025-10-14 21:19:58 +0700

    Add Example4Sum1ToN program to calculate sum from 1 to N

---
+++ src/part4.loops/Example4Sum1ToN/src/Example4Sum1ToN.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Example4Sum1ToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= N; i++) sum += i;
+        System.out.println(sum);
+    }
+}
