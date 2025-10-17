be68a5935c082b9cefabcdf2008d1f2c840265ba
nosatmcc <tacongson2003@gmail.com>
2025-10-17 22:54:48 +0700

    Add example to input and print N-element array

---
+++ src/part5.arrays/ArrayExample2/src/ArrayExample2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ArrayExample2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) System.out.print(a[i] + " ");
+    }
+}
