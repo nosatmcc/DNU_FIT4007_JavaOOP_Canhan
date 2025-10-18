278fd8ca2e2cc10a347105fa0a3a833f8edb9dee
nosatmcc <tacongson2003@gmail.com>
2025-10-18 12:55:52 +0700

    Add example to calculate sum of array elements

---
+++ src/part5.arrays/ArrayExample4/src/ArrayExample4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ArrayExample4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), sum = 0;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+}
