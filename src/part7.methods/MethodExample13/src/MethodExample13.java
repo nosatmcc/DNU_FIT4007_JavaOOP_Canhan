46fc3a6b4f2c415f66ed3477454ef5c61f4c8f41
nosatmcc <tacongson2003@gmail.com>
2025-10-20 22:32:49 +0700

    Add method to calculate area of triangle from 3 sides

---
+++ src/part7.methods/MethodExample13/src/MethodExample13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MethodExample13 {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+        System.out.println(area(a, b, c));
+    }
+}
