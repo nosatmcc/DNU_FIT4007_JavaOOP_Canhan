266fa2d0fbd739531e829bc0c327a65c3d2df4ee
nosatmcc <tacongson2003@gmail.com>
2025-10-20 22:04:41 +0700

    Add method to find greatest common divisor

---
+++ src/part7.methods/MethodExample9/src/MethodExample9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MethodExample9 {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(gcd(a, b));
+    }
+}
