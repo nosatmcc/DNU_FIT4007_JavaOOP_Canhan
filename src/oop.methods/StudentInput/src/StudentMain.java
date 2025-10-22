ecabaaa04230e976ef940bc06fd99b858964d37f
nosatmcc <tacongson2003@gmail.com>
2025-10-23 00:07:19 +0700

    Add Student class with input method for attributes

---
+++ src/oop.methods/StudentInput/src/StudentMain.java
@@ -0,0 +1,7 @@
+public class StudentMain {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.input();
+        System.out.println("Student info: " + s);
+    }
+}
