f8cc984da78c003a9a38f3549302eb904e315ed9
nosatmcc <tacongson2003@gmail.com>
2025-10-22 17:32:59 +0700

    Add Square class with area, perimeter, and integer side check methods

---
+++ src/oop.methods/Square/src/Square.java
@@ -0,0 +1,30 @@
+public class Square {
+    private double side;
+
+    public Square() {}
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getArea() {
+        return side * side;
+    }
+
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" + "side=" + side + '}';
+    }
+}
---
+++ src/oop.methods/Square/src/SquareMain.java
@@ -0,0 +1,8 @@
+public class SquareMain {
+    public static void main(String[] args) {
+        Square s = new Square(6);
+        System.out.println(s);
+        System.out.println("Area: " + s.getArea());
+        System.out.println("Perimeter: " + s.getPerimeter());
+    }
+}
