928bf958170af88ad13a94e97f5465599b5bbe03
nosatmcc <tacongson2003@gmail.com>
2025-10-22 16:12:29 +0700

    Add Circle class with area and perimeter calculation methods

---
+++ src/oop.methods/Circle/src/CircleMain.java
@@ -0,0 +1,8 @@
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle c = new Circle(3.5);
+        System.out.println(c);
+        System.out.println("Area: " + c.getArea());
+        System.out.println("Perimeter: " + c.getPerimeter());
+    }
+}
